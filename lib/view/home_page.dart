e0b999c864e25c1bb39591e0343cd287ec364c15
Esmaeil Ahmadipour <software8686@gmail.com>
2020-02-29 08:01:10 +0330

    Commit> Design UI

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,9 +1,9 @@
-
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
   final String title;
-  HomePage({this.title});
+
+  HomePage({this.title, Key key}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
@@ -12,6 +12,21 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children: <Widget>[
+          TextField(),
+          TextField(),
+          RaisedButton(
+            onPressed: (){},
+            child: Icon(Icons.add),
+          ),
+          Text("Result:",style: TextStyle(fontSize: 30.0),),
+        ],
+      ),
+    );
   }
 }
